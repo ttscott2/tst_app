4629efc565d225395aacd1fda3f5e6d914e15a1b
ttscott2 <timothytscott3@gmail.com>
2016-07-05 18:06:01 -0500

    add students controller

--- spec/features/list_students_spec.rb
+++ spec/features/list_students_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 describe "Viewing the list of students" do
   it "shows all students" do
-    visit "http://example.com/students"
+    visit students_url
 
     expect(page).to have_text("3 Students")
   end
